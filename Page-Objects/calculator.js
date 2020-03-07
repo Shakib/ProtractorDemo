5bb5670c47f65acface8edafeb519751bd32d52d
ShakibN <shakib_norzad@live.se>
2020-03-07 18:24:07 +0100

    second commit

--- Page-Objects/calculator.js
+++ Page-Objects/calculator.js
@@ -1,19 +1,22 @@
 import { Browser } from "selenium-webdriver"
+let basepage = require('../Page-Objects/BasePage');
 
 describe('Demo calculator test', () => {
 
     it('addition test', () => {
 
-        Browser.get('https://juliemr.github.io/protractor-demo/');
+       // Browser.get('https://juliemr.github.io/protractor-demo/');
+        basepage.get('https://juliemr.github.io/protractor-demo/');
 
         element(by.model('first')).sendKeys('2');
+        
         element(by.model('second')).sendKeys('3');
         element(by.css('[ng-click="doAddition()"]')).click();
 
         let result = element(by.cssContainingText('.ng-binding', '5'));
         expect(result.getText()).toEqual('5');
 
-        Browser.sleep(2000);
+        Browser.sleep(1000);
 
     })
 })
