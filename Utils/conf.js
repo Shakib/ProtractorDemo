6fa343a00af8e64615c42b26f7321ace8ba67570
ShakibN <shakib_norzad@live.se>
2020-03-08 02:25:57 +0100

    changed back to example_spec.js to run

--- Utils/conf.js
+++ Utils/conf.js
@@ -1,3 +1,14 @@
+
+// To create screeshot go to https://www.npmjs.com/package/protractor-jasmine2-screenshot-reporter and copy the code and past down here
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'target/screenshots',
+  filename: 'my-report.html'
+}); // and the rest copy goes under jasmineNodeOptions
+
+
+
 // An example configuration file.
 exports.config = {
   directConnect: true,
@@ -17,5 +28,24 @@ exports.config = {
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+
+   // Setup the report before any tests start
+   beforeLaunch: function() {
+    return new Promise(function(resolve){
+      reporter.beforeLaunch(resolve);
+    });
+  },
+
+  // Assign the test reporter to each running instance
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(reporter);
+  },
+
+  // Close the report after all tests finish
+  afterLaunch: function(exitCode) {
+    return new Promise(function(resolve){
+      reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
   }
 };
